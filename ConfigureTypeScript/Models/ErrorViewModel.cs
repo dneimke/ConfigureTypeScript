c6dc232333d46afcc2207e9673f283758d3e2380
Darren Neimke <darren.neimke@gmail.com>
2018-12-01 11:13:40 +1030

    Add project files.

---
+++ ConfigureTypeScript/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ConfigureTypeScript.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
